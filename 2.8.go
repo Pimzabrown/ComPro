1eb558e5db020879cb4be2f42ee6b56dd6eff9b5
Pimzabrown <75247304+Pimzabrown@users.noreply.github.com>
2020-12-06 23:42:11 +0700

    Add files via upload

---
+++ 2.8.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(986547 == 986547)
+	fmt.Println(145 > 657)
+	fmt.Println(89 < 89)
+	fmt.Println(789 <= 36)
+	fmt.Println(89 != 789)
+	fmt.Println("45 + 89 + 127 = ",45 + 89 + 127)
+}
