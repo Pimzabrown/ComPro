102511bdc8928b9b327886dcde9379a7fc8e5df1
Pimzabrown <75247304+Pimzabrown@users.noreply.github.com>
2020-12-06 23:41:01 +0700

    Add files via upload

---
+++ 2.4.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main()  {
+	fmt.Print(`\n \t Backticks`)
+	fmt.Print("\n \t Double quote")
+	fmt.Println()
+	fmt.Println(`\n \t Backticks` [8])
+	fmt.Println(len(`\n \t Backticks`))
+	fmt.Println("Phimchanok" + "Limpongsa" + "Pim" + "22/11")
+}
