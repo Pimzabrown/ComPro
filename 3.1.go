bd61323efdaf08bd266bc6c62752cba22a2d3a60
Pimzabrown <75247304+Pimzabrown@users.noreply.github.com>
2020-12-12 23:25:24 +0700

    Add files via upload

---
+++ 3.1.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+func main() {
+	student_1 := "MARK"
+	student_2 := "JAEMIN"
+	student_3 := "TEN"
+	student_4 := "JAEHYUN"
+	student_5 := "SHOTARO"
+	student_6 := "KUN"
+	student_7 := "YANGYANG"
+	student_8 := "JENO"
+	student_9 := "JOHNNY"
+	student_10 := "HAECHAN"
+	student_11 := "WINWIN"
+	student_12 := "LUCAS"
+	student_13 := "YUTA"
+	student_14 := "TAEIL"
+	student_15 := "RENJUN"
+	student_16 := "JISUNG"
+	student_17 := "TAEYONG"
+	student_18 := "JUNGWOO"
+	student_19 := "DOYOUNG"
+	student_20 := "CHENLE"
+	student_21 := "SUNGCHAN"
+	student_22 := "XIAOJUN"
+	student_23 := "HENDERY"
+
+	fmt.Println(student_1, student_2, student_3, student_4, student_5, student_6, student_7, student_8, student_9, student_10, student_11, student_12, student_13, student_14, student_15, student_16, student_17, student_18, student_19, student_20, student_21, student_22, student_23)
+}
