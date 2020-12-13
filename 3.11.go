976fa752670085fc2e2d9de72fde81b131e5ec37
Pimzabrown <75247304+Pimzabrown@users.noreply.github.com>
2020-12-13 23:49:19 +0700

    Add files via upload

---
+++ 3.11.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	n, e := fmt.Print("BEAUTIFUL", "NEZUKO", 874, 98745623, "\n")
+	fmt.Print("number :", n, "\n")
+	fmt.Print("write error encountered :", e, "\n")
+
+}
