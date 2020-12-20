d0e7e9bf240c04060e60c37272b6d7989a7185dd
Pimzabrown <75247304+Pimzabrown@users.noreply.github.com>
2020-12-20 23:16:41 +0700

    Add files via upload

---
+++ 4.15.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(mark)
+	fmt.Println(taeyong)
+	fmt.Println(jaemin)
+	fmt.Println(jeno)
+	fmt.Println(johnny)
+	fmt.Println(kun)
+	fmt.Println(doyoung)
+	fmt.Println(renjun)
+	fmt.Println(chenle)
+	fmt.Println(jisung)
+	fmt.Println(haechan)
+	fmt.Println(shotaro)
+	fmt.Println(sungchang)
+	fmt.Println(jaehyun)
+	fmt.Println(yuta)
+	fmt.Println(hendery)
+	fmt.Println(ten)
+	fmt.Println(taeil)
+	fmt.Println(xiaojun)
+	fmt.Println(winwin)
+	fmt.Println(jungwoo)
+	fmt.Println(lucas)
+	fmt.Println(yangyang)
+}
