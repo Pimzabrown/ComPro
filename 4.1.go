b7204d89e7c24cee129978a5225db6789c83c9da
Pimzabrown <75247304+Pimzabrown@users.noreply.github.com>
2020-12-20 23:12:14 +0700

    Add files via upload

---
+++ 4.1.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(12)
+	fmt.Println(13)
+	fmt.Println(14)
+	fmt.Println(15)
+	fmt.Println(16)
+	fmt.Println(17)
+	fmt.Println(18)
+	fmt.Println(19)
+	fmt.Println(20)
+	fmt.Println(21)
+	fmt.Println(22)
+	fmt.Println(23)
+	fmt.Println(24)
+	fmt.Println(25)
+}
