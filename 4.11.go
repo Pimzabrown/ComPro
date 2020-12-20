f7124cd53cc8478cdc0fdc25808cfb39bd4569ee
Pimzabrown <75247304+Pimzabrown@users.noreply.github.com>
2020-12-20 23:15:42 +0700

    Add files via upload

---
+++ 4.11.go
@@ -0,0 +1,211 @@
+package main
+
+import "fmt"
+
+func main() {
+	number := 1
+	switch number {
+	case 0:
+		fmt.Println("ZERO")
+	case 1:
+		fmt.Println("ONE")
+	case 2:
+		fmt.Println("TWO")
+	case 3:
+		fmt.Println("THREE")
+	case 4:
+		fmt.Println("FOUR")
+	case 5:
+		fmt.Println("FIVE")
+	case 6:
+		fmt.Println("SIX")
+	case 7:
+		fmt.Println("SEVEN")
+	case 8:
+		fmt.Println("EIGHT")
+	case 9:
+		fmt.Println("NINE")
+	case 10:
+		fmt.Println("TEN")
+	case 11:
+		fmt.Println("ELEVEN")
+	case 12:
+		fmt.Println("TWELVE")
+	case 13:
+		fmt.Println("THIRTEEN")
+	case 14:
+		fmt.Println("FOURTEEN")
+	case 15:
+		fmt.Println("FIFTEEN")
+	case 16:
+		fmt.Println("SIXTEEN")
+	case 17:
+		fmt.Println("SEVENTEEN")
+	case 18:
+		fmt.Println("EIGHTEEN")
+	case 19:
+		fmt.Println("NINETEEN")
+	case 20:
+		fmt.Println("TWENTY")
+	case 21:
+		fmt.Println("TWENTY-ONE")
+	case 22:
+		fmt.Println("TWENTY-TWO")
+	case 23:
+		fmt.Println("TWENTY-THREE")
+	case 24:
+		fmt.Println("TWENTY-FOUR")
+	case 25:
+		fmt.Println("TWENTY-FIVE")
+	case 26:
+		fmt.Println("TWENTY-SIX")
+	case 27:
+		fmt.Println("TWENTY-SEVEN")
+	case 28:
+		fmt.Println("TWENTY-EIGHT")
+	case 29:
+		fmt.Println("TWENTY-NINE")
+	case 30:
+		fmt.Println("THIRTY")
+	case 31:
+		fmt.Println("THIRTY-ONE")
+	case 32:
+		fmt.Println("THIRTY-TWO")
+	case 33:
+		fmt.Println("THIRTY-THREE")
+	case 34:
+		fmt.Println("THIRTY-FOUR")
+	case 35:
+		fmt.Println("THIRTY-FIVE")
+	case 36:
+		fmt.Println("THRITY-SIX")
+	case 37:
+		fmt.Println("THIRTY-SEVEN")
+	case 38:
+		fmt.Println("THIRTY-EIGHT")
+	case 39:
+		fmt.Println("THIRTY-NINE")
+	case 40:
+		fmt.Println("FORTY")
+	case 41:
+		fmt.Println("FORTY-ONE")
+	case 42:
+		fmt.Println("FORTY-TWO")
+	case 43:
+		fmt.Println("FURTY-THREE")
+	case 44:
+		fmt.Println("FORTY-FOUR")
+	case 45:
+		fmt.Println("FORTY-FIVE")
+	case 46:
+		fmt.Println("FORTY-SIX")
+	case 47:
+		fmt.Println("FORTY-SEVEN")
+	case 48:
+		fmt.Println("FORTY-EIGHT")
+	case 49:
+		fmt.Println("FORTY-NINE")
+	case 50:
+		fmt.Println("FIFTY")
+	case 51:
+		fmt.Println("FIFTY-ONE")
+	case 52:
+		fmt.Println("FIFTY-TWO")
+	case 53:
+		fmt.Println("FIFTY-THREE")
+	case 54:
+		fmt.Println("FIFTY-FOUR")
+	case 55:
+		fmt.Println("FIFTY-FIVE")
+	case 56:
+		fmt.Println("FIFTY-SIX")
+	case 57:
+		fmt.Println("FIFTY-SEVEN")
+	case 58:
+		fmt.Println("FIFTY-EIGHT")
+	case 59:
+		fmt.Println("FIFTY-NINE")
+	case 60:
+		fmt.Println("SIXTY")
+	case 61:
+		fmt.Println("SIXTY-ONE")
+	case 62:
+		fmt.Println("SIXTY-TWO")
+	case 63:
+		fmt.Println("SIXTY-THREE")
+	case 64:
+		fmt.Println("SIXTY-FOUR")
+	case 65:
+		fmt.Println("SIXTY-FIVE")
+	case 66:
+		fmt.Println("SIXTY-SIX")
+	case 67:
+		fmt.Println("SIXTY-SEVEN")
+	case 68:
+		fmt.Println("SIXTY-EIGHT")
+	case 69:
+		fmt.Println("SIXTY-NINE")
+	case 70:
+		fmt.Println("SEVENTY")
+	case 71:
+		fmt.Println("SEVENTY-ONE")
+	case 72:
+		fmt.Println("SEVENTY-TWO")
+	case 73:
+		fmt.Println("SEVENTY-THREE")
+	case 74:
+		fmt.Println("SEVENTY-FOUR")
+	case 75:
+		fmt.Println("SEVENTY-FIVE")
+	case 76:
+		fmt.Println("SEVENTY-SIX")
+	case 77:
+		fmt.Println("SEVENTY-SEVEN")
+	case 78:
+		fmt.Println("SEVENTY-EIGHT")
+	case 79:
+		fmt.Println("SEVENTY-NINE")
+	case 80:
+		fmt.Println("EIGHTY")
+	case 81:
+		fmt.Println("EIGHTY-ONE")
+	case 82:
+		fmt.Println("EIGHTY-TWO")
+	case 83:
+		fmt.Println("EIGHTY-THREE")
+	case 84:
+		fmt.Println("EIGHTY-FOUR")
+	case 85:
+		fmt.Println("EIGHTY-FIVE")
+	case 86:
+		fmt.Println("EIGHTY-SIX")
+	case 87:
+		fmt.Println("EIGHTY-SEVEN")
+	case 88:
+		fmt.Println("EIGHTY-EIGHT")
+	case 89:
+		fmt.Println("EIGHTY-NINE")
+	case 90:
+		fmt.Println("NINETY")
+	case 91:
+		fmt.Println("NINETY-ONE")
+	case 92:
+		fmt.Println("NINETY-TWO")
+	case 93:
+		fmt.Println("NINETY-THREE")
+	case 94:
+		fmt.Println("NINETY-FOUR")
+	case 95:
+		fmt.Println("NINETY-FIVE")
+	case 96:
+		fmt.Println("NINETY-SIX")
+	case 97:
+		fmt.Println("NINETY-SEVEN")
+	case 98:
+		fmt.Println("NINETY-EIGHT")
+	case 99:
+		fmt.Println("NINETY-NINE")
+	default:
+		fmt.Println("unknow")
+	}
+}
