e5a8a06560d0305f0e063207b44da018b4de28d4
Pimzabrown <75247304+Pimzabrown@users.noreply.github.com>
2021-01-03 22:50:22 +0700

    Add files via upload

---
+++ 6.12.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+func makeEven() func() int {
+	even := 0 
+	return func() int {
+		even = even + 15
+		return even
+	}
+}
+
+func main() {
+	nextEven := makeEven()
+	fmt.Println(nextEven())
+	fmt.Println(nextEven())
+	fmt.Println(nextEven())
+	fmt.Println(nextEven())
+	fmt.Println(nextEven())
+	fmt.Println(nextEven())
+	fmt.Println(nextEven())
+	fmt.Println(nextEven())
+	fmt.Println(nextEven())
+	fmt.Println(nextEven())
+	fmt.Println(nextEven())
+	fmt.Println(nextEven())
+	fmt.Println(nextEven())
+	fmt.Println(nextEven())
+	fmt.Println(nextEven())
+	fmt.Println(nextEven())
+}
