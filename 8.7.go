6e91e79281ee89c1b588bfb783f8f4d59745f2ee
Pimzabrown <75247304+Pimzabrown@users.noreply.github.com>
2021-01-17 18:37:00 +0700

    Add files via upload

---
+++ 8.7.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	student_1 := "Todoroki"
+	student_2 := "Marklee"
+	student_3 := "Barbie"
+	student_4 := "Shinobu"
+	student_5 := "Moomin"
+	student_6 := "Papamoomin"
+	student_7 := "Allmight"
+	student_8 := "Midnight"
+	student_9 := "Shotaro"
+	student_10 := "Tanjiro"
+
+	fmt.Println(student_1, student_2, student_3, student_4, student_5, student_6, student_7, student_8, student_9, student_10)
+}
