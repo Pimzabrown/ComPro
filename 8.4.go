484b67da453c707283456fd21a077f8a9d5700b2
Pimzabrown <75247304+Pimzabrown@users.noreply.github.com>
2021-01-17 18:36:20 +0700

    Add files via upload

---
+++ 8.4.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main(){
+	fmt.Println(true || false)
+	fmt.Println(true && true)
+	fmt.Println(!false)
+	fmt.Println(!true)
+	fmt.Println("PHIMCHANOK LIMPONGSA")
+	fmt.Println("K-9 is my puppy")
+	fmt.Println("Green Tea Milk is Sweet more than Cocoa Ice")
+	fmt.Println(56 + 50 + 189 + 54 - 99 * 233 / 78 * 12 - 15 * 168)
+	fmt.Println(1 <= 6)
+	fmt.Println(22 == 22)
+	fmt.Println(63 >= 99)
+	fmt.Println(5 > 4)
+	fmt.Println(9 != 87)
+}
