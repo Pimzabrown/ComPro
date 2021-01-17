efd466658dcee1900fd6d829886567f79cce2ffb
Pimzabrown <75247304+Pimzabrown@users.noreply.github.com>
2021-01-17 18:37:50 +0700

    Add files via upload

---
+++ 8.10.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	numbers := [25]int{1, 2 , 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25}
+	primes := [12]int{25, 86, 14, 19, 23, 20, 34, 47, 68, 45, 88, 10}
+
+	fmt.Println(numbers[22])
+	fmt.Println(primes[2])
+}
