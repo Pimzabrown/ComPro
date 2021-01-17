2a305f20b0cb38d049053fc7bdde12766589951e
Pimzabrown <75247304+Pimzabrown@users.noreply.github.com>
2021-01-17 18:37:16 +0700

    Add files via upload

---
+++ 8.8.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func main() {
+	student := []string{"Todoroki", "Marklee", "Barbie","Allmight", "Shotaro", "Moomin", "Papamoomin", "Shinobu", "Tanjiro", "Midnight"}
+
+	fmt.Println(student)
+}
