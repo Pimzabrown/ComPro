a19e5e2d060b53aa2206860da9c47d05e85b077e
Pimzabrown <75247304+Pimzabrown@users.noreply.github.com>
2021-01-17 18:36:05 +0700

    Add files via upload

---
+++ 8.3.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello My name is Phimchanok Limpongsa")
+	fmt.Println("My nickname is Pim")
+	fmt.Println("My blood type is A")
+	fmt.Println("My birthday is 22 November 1999. My age is 21 years old.")
+	fmt.Println("My idol is Marklee and Allmight and Todorogi Shoto.")
+	fmt.Println(231 + 15 - 78 - 45 * 78 / 4 / 69 + 14 - 22 * 4)
+}
