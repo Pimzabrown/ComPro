3b0eefaa351017e48f4ef20818ac21e2ee857b22
Pimzabrown <75247304+Pimzabrown@users.noreply.github.com>
2021-01-24 23:11:49 +0700

    Add files via upload

---
+++ 9.1.go
@@ -0,0 +1,32 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello K-9")
+	fmt.Println(9 * 87)
+	fmt.Println("Everything is easy")
+	fmt.Println(12 - 25)
+	fmt.Println("PHIMCHANOK LIMPONGSA")
+	fmt.Println(35 / 7)
+	fmt.Println("I'am Deku")
+	fmt.Println(25 + 43 - 87 * 102 / 36)
+	fmt.Println("My Hero Academia")
+	fmt.Println(28 % 4)
+	fmt.Println("U.A. Academia")
+	fmt.Println(true && true)
+	fmt.Println(!false)
+	fmt.Println(!true)
+	fmt.Println(false || true)
+	fmt.Println(true || false)
+	fmt.Println(false && false)
+	fmt.Println(true || true)
+	fmt.Println(false || false)
+	fmt.Println(2 < 5669)
+	fmt.Println(87 != 78)
+	fmt.Println(991 <= 991)
+	fmt.Println(23 > 10)
+	fmt.Println(741 >= 741)
+	fmt.Println(1 != 1)
+	fmt.Println("When we shine bright")
+}
