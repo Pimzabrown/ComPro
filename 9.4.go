b8eaae3763570cdcc02b294f1c8d3a9a9399e224
Pimzabrown <75247304+Pimzabrown@users.noreply.github.com>
2021-01-24 23:12:35 +0700

    Add files via upload

---
+++ 9.4.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	names := []string{}
+	age := []string{}
+	names = append(names, "DEKU")
+	age = append(age, "15")
+	fmt.Println(names)
+	fmt.Println(age)
+}
